7431b09cc53a7bd1c5c0b43035767dd223af0760
ygbekou <ericgbekou@hotmail.com>
2019-01-31 15:39:59 -0600

    Changes for the sliders

--- src/app/components/website/header.ts
+++ src/app/components/website/header.ts
@@ -7,9 +7,7 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 @Component({
   selector: 'app-web-header',
   template: `
-            <header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true,
-                        'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true,
-                        'stickyStartAt': 120, 'stickyHeaderContainerHeight': 70}">
+			<header id="header" class="header-effect-shrink" data-plugin-options="{'stickyEnabled': true, 'stickyEffect': 'shrink', 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyChangeLogo': true, 'stickyStartAt': 120, 'stickyHeaderContainerHeight': 70}">
 				<div class="header-body border-color-primary header-body-bottom-border">
 					<div class="header-top header-top-default border-bottom-0">
 						<div class="container">
@@ -30,24 +28,20 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 										</nav>
 									</div>
                                 </div>
-                                <div>
+                                <div class="header-column justify-content-end">
                                     <div class="header-row">
                                         <nav class="header-nav-top">
-                                            {{'COMMON.LANGUAGE' | translate}} &nbsp; &nbsp;
-                                            <ul class="nav nav-pills text-uppercase text-2">
-                                                <li class="nav-item nav-item-anim-icon d-none d-md-block">
-                                                    <p-radioButton name="language" value="en" label="EN"
-                                                     [(ngModel)]="globalEventsManager.currentLang" #langSelect="ngModel"
-                                                    (click)="globalEventsManager.changeLanguage('en')">
-                                                    </p-radioButton>&nbsp;&nbsp;&nbsp;&nbsp;
-                                                </li>
-                                                <li class="nav-item nav-item-anim-icon d-none d-md-block">
-                                                    <p-radioButton name="language" value="fr" label="FR"
-                                                    [(ngModel)]="globalEventsManager.currentLang" #langSelect="ngModel"
-                                                    (click)="globalEventsManager.changeLanguage('fr')">
-                                                    </p-radioButton>
-                                                </li>
-											</ul>
+                                            &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <p-radioButton name="language" value="en" label="EN"
+                                                [(ngModel)]="globalEventsManager.currentLang" #langSelect="ngModel"
+                                            (click)="globalEventsManager.changeLanguage('en')">
+                                            </p-radioButton>&nbsp;&nbsp;&nbsp;&nbsp;
+
+                                            <p-radioButton name="language" value="fr" label="FR"
+                                            [(ngModel)]="globalEventsManager.currentLang" #langSelect="ngModel"
+                                            (click)="globalEventsManager.changeLanguage('fr')">
+                                            </p-radioButton>
+
 										</nav>
 									</div>
                                 </div>
@@ -56,11 +50,12 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 										<nav class="header-nav-top">
 											<ul class="nav nav-pills">
 												<li class="nav-item">
-                                                    <a href="mailto:{{company.email}}"><i class="far fa-envelope text-4
+													<a href="mailto:{{company.email}}"><i class="far fa-envelope text-4
                                                     text-color-primary" style="top: 1px;"></i> {{company.email}}</a>
 												</li>
 												<li class="nav-item">
-													<a href="tel:{{company.phone}}"><i class="fab fa-whatsapp text-4 text-color-primary" style="top: 0;"></i> {{company.phone}}</a>
+                                                    <a href="tel:{{company.phone}}"><i class="fab fa-whatsapp text-4
+                                                    text-color-primary" style="top: 0;"></i> {{company.phone}}</a>
 												</li>
 											</ul>
 										</nav>
@@ -74,8 +69,8 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 							<div class="header-column">
 								<div class="header-row">
 									<div class="header-logo">
-										<a href="/">
-                                            <img alt="ATC" width="200" height="96" src="assets/docs/Company/{{company.logo}}">
+										<a href="index.html">
+											<img alt="ATC" width="200" height="96" src="assets/docs/Company/{{company.logo}}">
 										</a>
 									</div>
 								</div>
@@ -131,12 +126,14 @@ import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 										</div>
 										<ul class="header-social-icons social-icons d-none d-sm-block">
                                             <li class="social-icons-facebook"><a href="{{company.facebookUrl}}" target="_blank"
-                                            title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                                title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
                                             <li class="social-icons-twitter"><a href="{{company.twitterUrl}}" target="_blank"
-                                            title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                                                title="Twitter"><i class="fab fa-twitter"></i></a></li>
                                             <li class="social-icons-linkedin"><a href="{{company.linkedInUrl}}" target="_blank"
-                                            title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
-										</ul>
+                                                title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>										</ul>
+										<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
 									</div>
 								</div>
 							</div>
